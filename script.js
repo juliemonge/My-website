8b58a51ed2101a9c80354c4bc3a6b8467f1b1953
@Julie.Hallan <juliemh@live.no>
2024-06-17 14:38:54 +0200

    blogpost

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     alert('Welcome to My Simple Website!');
 });
+
+const fileUrl = /Users/juliemongehallan/Documents/Datatek/My Site/blog.rtf // provide file location
+
+fetch(fileUrl)
+   .then( r => r.text() )
+   .then( t => console.log(t) )
